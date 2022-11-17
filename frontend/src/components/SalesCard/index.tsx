00da302f0a8eefb9218bfc08d79aa836efc61bce
Eduardo <eduardo.annes16@gmail.com>
2022-11-16 23:01:05 -0300

    Changes

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -6,33 +6,39 @@ import './styles.css';
 
 function SalesCard() {
     
-    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const d = new Date();
+    const currMonth = d.getMonth();
+    const currYear = d.getFullYear();
+    const min = new Date(currYear, currMonth, 1);
     const max = new Date();
-
+  
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
-
     return (
-        <div className="dsmeta-card">
-            <h2 className="dsmeta-sales-title">Vendas</h2>
-            <div>
-                <div className="dsmeta-form-control-container">
-                    <DatePicker
-                        selected={minDate}
-                        onChange={(date: Date) => setMinDate(date)}
-                        className="dsmeta-form-control"
-                        dateFormat="dd/MM/yyyy"
-                    />
-                </div>
-                <div className="dsmeta-form-control-container">
-                    <DatePicker
-                        selected={maxDate}
-                        onChange={(date: Date) => setMaxDate(date)}
-                        className="dsmeta-form-control"
-                        dateFormat="dd/MM/yyyy"
-                    />
-                </div>
-            </div>
+      <div className="dsmeta-card">
+        <h2 className="dsmeta-sales-title">Vendas</h2>
+        <div>
+          <div className="dsmeta-form-control-container">
+            <DatePicker
+              selected={minDate}
+              onChange={(date: Date) => {
+                setMinDate(date);
+              }}
+              className="dsmeta-form-control"
+              dateFormat="dd/MM/yyyy"
+            />
+          </div>
+          <div className="dsmeta-form-control-container">
+            <DatePicker
+              selected={maxDate}
+              onChange={(date: Date) => {
+                setMaxDate(date);
+              }}
+              className="dsmeta-form-control"
+              dateFormat="dd/MM/yyyy"
+            />
+          </div>
+        </div>
 
             <div>
                 <table className="dsmeta-sales-table">
--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -4,9 +4,9 @@ import './styles.css';
 function Header() {
     return( 
         <header>
-        <div className="edmeta-logo-container">
-            <img src={logo} alt="EDMeta" />
-            <h1>EDMeta</h1>
+        <div className="dsmeta-logo-container">
+            <img src={logo} alt="MetaDados" />
+            <h1>Meta Dados</h1>
             <p>
               Desenvolvido por 
               <a href="https://www.instagram.com/edu_diniz2953"> Eduardo Diniz</a>
